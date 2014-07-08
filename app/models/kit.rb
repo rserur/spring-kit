ce908e95cf921155a1c0d81b9f0099b0e612be2a
Rachael Serur <rachaelse@gmail.com>
2014-07-08 11:06:34 -0400

    Devise set up

---
+++ app/models/kit.rb
@@ -0,0 +1,3 @@
+class Kit < ActiveRecord::Base
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "home#index"
+
+  root "pages#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
