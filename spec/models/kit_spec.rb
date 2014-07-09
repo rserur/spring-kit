051149239613afee0b332bae8b8da74971d63431
Rachael Serur <rachaelse@gmail.com>
2014-07-09 14:46:16 -0400

    Kit and user model specs written.

--- spec/models/kit_spec.rb
+++ spec/models/kit_spec.rb
@@ -8,8 +8,8 @@ describe Kit do
   it { should belong_to(:client).class_name('User') }
   it { should belong_to(:practitioner).class_name('User') }
 
-  it {should_not have_valid(:client_id).when(nil)}
-  it {should_not have_valid(:practitioner_id).when(nil)}
+  it { should_not have_valid(:client_id).when(nil) }
+  it { should_not have_valid(:practitioner_id).when(nil) }
 
   it { should validate_uniqueness_of(:client_id).scoped_to(:practitioner_id) }
 end
