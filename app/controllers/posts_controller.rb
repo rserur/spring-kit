6b83ab15c216fe919ba46ce337cfb2802b0e7d50
Rachael Serur <rachaelse@gmail.com>
2014-07-18 11:57:01 -0400

    Added phone number column to users.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -60,6 +60,24 @@ class PostsController < ApplicationController
     redirect_to kit_path(kit)
   end
 
+  # def send_text_message
+  #   number_to_send_to = params[:recipient_number]
+
+  #   twilio_sid = ENV['TWILIO_SID']
+  #   twilio_token = ENV['TWILIO_TOKEN']
+  #   twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
+
+  #   @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+  #   @twilio_client.account.sms.messages.create(
+  #     :from => "+1#{twilio_phone_number}",
+  #     :to => number_to_send_to,
+  #     :body => "This is an message. It gets sent to #{number_to_send_to}"
+  #   )
+  # end
+
+private
+
   def post_params
     params.require(:post).permit(
       :title, :body, :collection_list, :media, :message
